d5579b8fb506b3c80052f7d51070a3c62c563de9
Orgil Batbileg <osohitooss@gmail.com>
2025-12-07 21:35:12 +0800

    Add multiplication function with JUnit tests

---
+++ src/main/java/lab14/sict/must/edu/mn/Multiplication.java
@@ -0,0 +1,17 @@
+// src/main/java/lab14/sict/must/edu/mn/Multiplication.java
+package lab14.sict.must.edu.mn;
+
+/**
+ * Multiplication calculator.
+ */
+public class Multiplication {
+    /**
+     * Multiplies two numbers.
+     * @param a first number
+     * @param b second number
+     * @return product of a and b
+     */
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+}
